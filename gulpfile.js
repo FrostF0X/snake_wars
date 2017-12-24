5b758a50f200328f24c67653586353a2b803c85f
Dmytro Konovalov <dmytro.konowalow@gmail.com>
2017-12-24 10:33:54 +0100

    Initialize typescript configuration, babel scripts, tslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const ts = require('gulp-typescript').createProject('tsconfig.json');
+
 
 gulp.task('build', () =>
     gulp.src('app/**')
@@ -14,3 +16,14 @@ gulp.task('watch', () =>
 );
 
 gulp.task('default', ['watch']);
+
+
+gulp.task('build-ts', () =>
+    gulp.src('app-typescript/**')
+        .pipe(ts())
+        .pipe(gulp.dest('dist'))
+);
+
+gulp.task('watch-ts', () =>
+    gulp.watch('app-typescript/**', ['build'])
+);
