faae436519b2f1751951b31e40be26c37e0bc3b8
Dmytro Konovalov <dmytro.konowalow@gmail.com>
2017-12-26 18:34:24 +0100

    Remove JavaScript application sources and gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,7 @@
 const gulp = require('gulp');
-const babel = require('gulp-babel');
 const ts = require('gulp-typescript').createProject('tsconfig.json');
 
-
-gulp.task('build', () =>
-    gulp.src('app/**')
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(gulp.dest('dist'))
-);
-
-gulp.task('watch', () =>
-    gulp.watch('app/**', ['build'])
-);
-
-gulp.task('default', ['watch']);
-
+gulp.task('default', ['watch-ts']);
 
 gulp.task('build-ts', () =>
     gulp.src('app-typescript/**')
